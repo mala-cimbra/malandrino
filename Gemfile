5f9e12064c367012323ceaf036405d107935c0fb
Ioma Taani <iomataani@users.noreply.github.com>
2016-08-02 22:11:47 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://www.rubygems.org/'
+gem 'sinatra'
+gem 'thin'
+gem 'tilt', require: 'tilt/erubis'
+gem 'erubis'
+gem 'sqlite3'
---
+++ app.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'base64'
+require 'digest'
+
+require 'pp' # messaggi di debug
+
+#----------------
+# carica dinamicamente le librerie all'interno della cartella ./lib
+# lista i file e poi li richiama col require
+# libs è un array, visto che li tira fuori con lib/libreria.rb
+#ci aggiungo un ./ (poi magari alla fine non cambia un cazzo)
+libs = Dir.glob(File.join("lib", "*.rb")).map {|file| "./#{file}"}
+libs.each do |lib|
+    require lib
+end
+#----------------
+
+get '/' do
+  erb :index
+end
