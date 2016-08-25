d5f134e732d6c64f50f0187b95242e2135546263
Ioma Taani <iomataani@users.noreply.github.com>
2016-08-25 16:58:42 +0200

    beneficiario fatto

--- lib/beneficiario.rb
+++ lib/beneficiario.rb
@@ -22,10 +22,10 @@ post '/beneficiari/new' do
     indirizzo = params["addr"]      #
     note = params["note"]
 
-    dati_beneficiario = {numero_telefono: numero_telefono, email: email, indirizzo: indirizzo, note: note}
+    dati_beneficiario = {numero_telefono: numero_telefono, email: email, indirizzo: indirizzo, note: note}.to_json
     
     # metti nel db
-    $db.execute("INSERT INTO beneficiari VALUES(NULL, '#{nome_beneficiario}', '#{dati_beneficiario.to_json}');")
+    $db.execute("INSERT INTO beneficiari VALUES(NULL, '#{nome_beneficiario}', '#{dati_beneficiario}');")
     
     redirect to('/beneficiari')
 end
@@ -43,9 +43,34 @@ get '/beneficiari/show/:id' do |id|
 end
 
 get '/beneficiari/edit/:id' do |id|
-
+    dati_beneficiario = $db.execute("SELECT * FROM beneficiari WHERE id = #{id};")
+    
+    if dati_beneficiario.empty?
+        redirect to('/beneficiari')
+    else
+        @id_beneficiario = dati_beneficiario[0][0]
+        @nome_beneficiario = dati_beneficiario[0][1]
+        @hash_info = JSON.parse(dati_beneficiario[0][2])
+        erb :beneficiari_edit
+    end
 end
 
 post '/beneficiari/edit/:id' do |id|
-
+    cerca_beneficiario = $db.execute("SELECT * FROM beneficiari WHERE id=#{id};")
+    if cerca_beneficiario.empty?
+        redirect to('/beneficiari')
+    else
+        id_beneficiario = params["id"]
+        nome_beneficiario = params["nome_beneficiario"]
+        numero_telefono = params["tel"] #
+        email = params["email"]         #
+        indirizzo = params["addr"]      #
+        note = params["note"]
+        # Genera JSON
+        dati_beneficiario = {numero_telefono: numero_telefono, email: email, indirizzo: indirizzo, note: note}.to_json
+        
+        # salva sul DB
+        $db.execute("UPDATE beneficiari SET nome_beneficiario='#{nome_beneficiario}', dati_beneficiario='#{dati_beneficiario}' WHERE id=#{id_beneficiario};")
+        redirect to("/beneficiari/show/#{id}")
+    end
 end
