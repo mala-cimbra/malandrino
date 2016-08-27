10844c2e6524891eda006295eb07b61ed00a3c62
Ioma Taani <iomataani@users.noreply.github.com>
2016-08-27 10:35:49 +0200

    cominciano le categorie

--- lib/categoria.rb
+++ lib/categoria.rb
@@ -8,6 +8,28 @@ pagina delle categorie
 =end
 
 get '/categorie' do
+    @lista_categorie = $db.execute("SELECT * FROM categorie;")
+    if @lista_categorie.empty?
+        erb :"categorie/categorie_vuoto"
+    else
 
-    erb :"categorie/categorie"
+        erb :"categorie/categorie"
+    end
+end
+
+get '/categorie/new' do
+end
+
+post '/categorie/new' do
+end
+
+
+get '/categorie/show/:id' do |id|
+
+end
+
+get '/categorie/edit/:id' do |id|
+end
+
+post '/categorie/edit/:id' do |id|
 end
