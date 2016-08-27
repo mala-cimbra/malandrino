91700faa6dece3bd89a968dee6e505e99cac09a0
Ioma Taani <iomataani@users.noreply.github.com>
2016-08-27 18:12:30 +0200

    sistemare le categorie, ma quasi fatto

--- lib/categoria.rb
+++ lib/categoria.rb
@@ -2,9 +2,8 @@
 #--------
 =begin
 pagina delle categorie
-
-
-
+DATABASE
+ID - id_cat_padre - nome_categoria
 =end
 
 get '/categorie' do
@@ -18,9 +17,16 @@ get '/categorie' do
 end
 
 get '/categorie/new' do
+    erb :"categorie/categorie_new"
 end
 
 post '/categorie/new' do
+    lista_categorie = $db.execute("SELECT * FROM categorie;");
+    nome_categoria = params["nome_categoria"]
+    array_categoria = nome_categoria.split(">")
+
+
+    $db.execute("INSERT INTO categorie VALUES(NULL, #{id_cat_padre}, '#{nome_categoria}');")
 end
 
 
--- public/js/ui.js
+++ public/js/ui.js
@@ -33,3 +33,21 @@
     };
 
 }(this, this.document));
+
+
+/*
+ * funzione per la gestione del mostra/nascondi
+ * una qualsiasi div/cosa
+ */
+function toggle_visibility(id)
+{
+    var vis = document.getElementById(id);
+    if (vis.style.display == 'block')
+    {
+        vis.style.display = 'none';
+    }
+    else
+    {
+        vis.style.display = 'block';
+    }
+}
